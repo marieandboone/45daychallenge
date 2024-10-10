3ed52f0de0596daaf3f96960b51c1fae24d0fa77
marieandboone <160288419+marieandboone@users.noreply.github.com>
2024-10-10 11:09:10 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener("install", (event) => {
         "/index.html",
         "/style.css",
         "/app.js",
-        "/icon.png",
+        "/play_store_512.png",
       ]);
     })
   );
