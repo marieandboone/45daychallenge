22f05118594104bffeb6a30c5fb44ab570ffa02b
marieandboone <160288419+marieandboone@users.noreply.github.com>
2024-12-25 21:19:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,34 +4,9 @@ const calorieGoal = 70000;
 
 // Create a date object for September 22 of the current year
 let currentYear = new Date().getFullYear();
-let startDate = new Date(currentYear, 11, 16); // September is month 8 (0-indexed)
-let daysInCalendar = 45;
+let startDate = new Date(currentYear, 11, 27); // September is month 8 (0-indexed)
+let daysInCalendar = 40;
 
-// let existingCaloriesData = {
-//   "Monday, September 23, 2024": "2087",
-//   "Tuesday, September 24, 2024": "2349",
-//   "Wednesday, September 25, 2024": "1920",
-//   "Thursday, September 26, 2024": "1731",
-//   "Friday, September 27, 2024": "1848",
-//   "Saturday, September 28, 2024": "508",
-//   "Sunday, September 29, 2024": "514",
-//   "Monday, September 30, 2024": "1520",
-//   "Tuesday, October 1, 2024": "1550",
-//   "Wednesday, October 2, 2024": "2083",
-//   "Thursday, October 3, 2024": "1647",
-//   "Friday, October 4, 2024": "1228",
-//   "Saturday, October 5, 2024": "1618",
-//   "Sunday, October 6, 2024": "951",
-//   "Monday, October 7, 2024": "1756",
-//   "Tuesday, October 8, 2024": "2228",
-//   "Wednesday, October 9, 2024": "1945",
-//   "Thursday, October 10, 2024": "1225",
-//   "Friday, October 11, 2024": "2105",
-// };
-
-// if (!localStorage.getItem("caloriesData")) {
-//   localStorage.setItem("caloriesData", JSON.stringify(existingCaloriesData));
-// }
 function initializeRewards() {
   const storedRewards = localStorage.getItem("remainingRewards");
 
@@ -54,36 +29,16 @@ function loadCaloriesData() {
   }
 }
 
-// // Check if localStorage is already populated, otherwise initialize it
-// if (
-//   !localStorage.getItem("pendingRewards") ||
-//   !localStorage.getItem("completedRewards") ||
-//   !localStorage.getItem("remainingRewards")
-// ) {
-//   localStorage.setItem("pendingRewards", JSON.stringify(pending));
-//   localStorage.setItem("completedRewards", JSON.stringify(done));
-//   localStorage.setItem("remainingRewards", JSON.stringify(remaining));
-// }
-
 function updateCalorieGoal() {
-  // Sum the calories burned from all days
-  let totalCaloriesBurned = Object.values(caloriesData).reduce(
-    (total, dayData) => total + (Number(dayData.calories) || 0),
-    0
-  );
-
-  let poundsLeft = Math.round((totalCaloriesBurned / 3500) * 10) / 10;
-
-  // Calculate remaining calories to reach the goal
-  let remainingCaloriesCalc = calorieGoal - totalCaloriesBurned;
-  let remainingCalories = remainingCaloriesCalc > 0 ? remainingCaloriesCalc : 0;
+  let daysLeft = calculateRemainingDays();
+  let daysPassed = 41 - daysLeft;
 
   // Update the H2 element with the total burned and the remaining to the goal
-  let calorieGoalText = `Calories Burned: ${totalCaloriesBurned.toLocaleString()} / 70,000 (${remainingCalories.toLocaleString()} left)`;
+  let calorieGoalText = `Day: ${daysPassed} / 40`;
   document.getElementById("calorieGoal").textContent = calorieGoalText;
 
   // Update progress bar
-  let progressPercentage = (totalCaloriesBurned / calorieGoal) * 100;
+  let progressPercentage = (daysPassed / 40) * 100;
   let progressBar = document.getElementById("progress-bar");
   progressBar.style.width = `${progressPercentage}%`;
   progressBar.textContent = `${Math.round(progressPercentage)}%`;
@@ -93,23 +48,23 @@ function updateCalorieGoal() {
     let dayDate = dayDiv.title;
 
     if (caloriesData[dayDate]) {
-      const { calories, chordii, bike } = caloriesData[dayDate];
-      const caloriesMet = (Number(calories) || 0) >= 1500;
-      const chordiiMet = (Number(chordii) || 0) >= 50;
-      const bikeMet = (Number(bike) || 0) >= 15;
+      const { fast, chordii, pray } = caloriesData[dayDate];
+      const fastMet = fast === true;
+      const chordiiMet = chordii === true;
+      const prayMet = pray === true;
 
       // Update the dots display
       let caloriesDisplay = dayDiv.querySelector(".calories-display");
       caloriesDisplay.innerHTML = `
-        <span class="goal-dot ${caloriesMet ? "goal-met" : ""}"></span>
+        <span class="goal-dot ${fastMet ? "goal-met" : ""}"></span>
         <span class="goal-dot ${chordiiMet ? "goal-met" : ""}"></span>
-        <span class="goal-dot ${bikeMet ? "goal-met" : ""}"></span>
+        <span class="goal-dot ${prayMet ? "goal-met" : ""}"></span>
       `;
     }
   });
 
   // Calculate remaining days and update the info section
-  displayRemainingDays(remainingCaloriesCalc, poundsLeft);
+  displayRemainingDays();
 }
 
 // Function to calculate the number of days remaining from the 45 days
@@ -124,7 +79,7 @@ function calculateRemainingDays() {
   let daysPassed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
   // Subtract the days passed from the total 45 days
-  let remainingDays = 45 - daysPassed;
+  let remainingDays = 40 - daysPassed;
 
   // Ensure remaining days is never less than 0 (in case the current date is beyond the 45-day period)
   return remainingDays > 0 ? remainingDays : 0;
@@ -156,16 +111,12 @@ function addNotificationBadge(days) {
 }
 
 // Function to display remaining days on the page
-function displayRemainingDays(remainingCaloriesCalc, poundsLeft) {
+function displayRemainingDays() {
   // Calculate the remaining days
   let remainingDays = calculateRemainingDays();
-  let calsPerDay =
-    remainingDays > 0 ? remainingCaloriesCalc / remainingDays : 0;
 
   // Update the P element with the remaining cals per day
-  let remainingDaysText = `You've lost ${poundsLeft} pounds!<br><br> Burn ${Math.ceil(
-    calsPerDay
-  )} kcals/day to reach your goal.`;
+  let remainingDaysText = `${remainingDays} days left.<br><br> Keep going.`;
   document.getElementById("info").innerHTML = remainingDaysText;
 }
 
@@ -257,76 +208,20 @@ function openModal(selectedDate, dayIndex) {
   // Check if there are existing values for the selected date
   if (caloriesData[selectedDate]) {
     const dayData = caloriesData[selectedDate];
-    document.getElementById("calories").value = dayData.calories || "";
-    document.getElementById("chordii").value = dayData.chordii || "";
-    document.getElementById("bike").value = dayData.bike || "";
+    document.getElementById("fast").checked = dayData.fast || "";
+    document.getElementById("chordii").checked = dayData.chordii || "";
+    document.getElementById("pray").checked = dayData.pray || "";
   } else {
     // Clear the inputs if no data exists
-    document.getElementById("calories").value = "";
-    document.getElementById("chordii").value = "";
-    document.getElementById("bike").value = "";
+    document.getElementById("fast").checked = "";
+    document.getElementById("chordii").checked = "";
+    document.getElementById("pray").checked = "";
   }
 }
 
 // Function to close the modal
 function closeModal() {
   document.getElementById("calorieModal").style.display = "none";
-}
-
-// Function to sum weekly calories based on start date
-function weeklyTotals() {
-  if (!startDate) {
-    console.error(
-      "Challenge start date is not defined. Please generate the calendar first."
-    );
-    return;
-  }
-
-  const ul = document.getElementById("weekly-totals");
-  const weeks = 7; // Total number of weeks
-  const weekRanges = [];
-
-  // Generate weekly date ranges
-  for (let i = 0; i < weeks; i++) {
-    const weekStart = new Date(startDate);
-    weekStart.setDate(startDate.getDate() + i * 7);
-
-    const weekEnd = new Date(weekStart);
-    weekEnd.setDate(weekStart.getDate() + 6);
-
-    weekRanges.push({ weekStart, weekEnd });
-  }
-
-  // Calculate totals for each week
-  const weeklyResults = weekRanges.map(({ weekStart, weekEnd }) => {
-    let total = 0;
-
-    for (const dateStr in caloriesData) {
-      // Parse the date string into a Date object for comparison
-      const currentDate = new Date(dateStr);
-
-      if (currentDate >= weekStart && currentDate <= weekEnd) {
-        const dayData = caloriesData[dateStr];
-        total += parseInt(dayData.calories || 0, 10);
-      }
-    }
-
-    return total;
-  });
-
-  // Clear the existing list
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-
-  // Populate the weekly totals in the list
-  weeklyResults.forEach((total, index) => {
-    const li = document.createElement("li");
-    li.innerText = `Week ${index + 1}: ${total} kcals (${(total / 3500).toFixed(
-      1
-    )} lbs)`;
-    ul.appendChild(li);
-  });
 }
 
 function submitCalories() {
@@ -338,20 +233,19 @@ function submitCalories() {
     .getElementById("calorieModal")
     .getAttribute("data-day-index");
 
-  let calories = document.getElementById("calories").value;
-  let chordii = document.getElementById("chordii").value;
-  let bike = document.getElementById("bike").value;
+  let fast = document.getElementById("fast").checked;
+  let chordii = document.getElementById("chordii").checked;
+  let pray = document.getElementById("pray").checked;
 
-  if (calories > 0 || chordii > 0 || bike > 0) {
+  if (fast || chordii || pray) {
     caloriesData[selectedDate] = {
-      calories: calories,
+      fast: fast,
       chordii: chordii,
-      bike: bike,
+      pray: pray,
     };
     saveCaloriesData();
     updateCalorieGoal();
-    weeklyTotals();
-    updateChordiiSummary();
+    updateSummary();
 
     // Update rewards and notification badge
     let daysMeetingCriteria = countDaysMeetingCriteria();
@@ -366,9 +260,9 @@ function submitCalories() {
     closeModal();
 
     // Reset inputs
-    document.getElementById("calories").value = "";
-    document.getElementById("chordii").value = "";
-    document.getElementById("bike").value = "";
+    document.getElementById("fast").checked = false;
+    document.getElementById("chordii").checked = false;
+    document.getElementById("pray").checked = false;
   } else if (calories && calories === "0") {
     // Handle calorie deletion
     delete caloriesData[selectedDate];
@@ -377,7 +271,6 @@ function submitCalories() {
     let caloriesDisplay = dayDiv.querySelector(".calories-display");
     caloriesDisplay.textContent = "";
     updateCalorieGoal();
-    weeklyTotals();
 
     // Update rewards
     let daysMeetingCriteria = countDaysMeetingCriteria();
@@ -432,16 +325,14 @@ document
 function countDaysMeetingCriteria() {
   // Get the number of days with calories greater than or equal to 1500 from caloriesData
   let daysCount = Object.entries(caloriesData).filter(([date, values]) => {
-    const calories = Number(values.calories) || 0;
-    const chordii = Number(values.chordii) || 0;
-    const bike = Number(values.bike) || 0;
+    const fast = values.fast === true;
+    const chordii = values.chordii === true;
+    const pray = values.pray === true;
 
-    return calories >= 1500 && chordii >= 50 && bike >= 15;
+    return fast && chordii && pray;
   }).length;
 
-  console.log(
-    `Total number of days with calories greater than or equal to 1500: ${daysCount}`
-  );
+  console.log(`Total number of days with all three goals: ${daysCount}`);
   updateRewardsBasedOnDays(daysCount); // New function to handle reward updates
   return daysCount;
 }
@@ -489,8 +380,7 @@ window.onload = function () {
   initializeRewards();
   generate45DayCalendar();
   loadCaloriesData();
-  updateChordiiSummary();
-  weeklyTotals();
+  updateSummary();
   countDaysMeetingCriteria();
   let pendingRewards = JSON.parse(localStorage.getItem("pendingRewards")) || [];
   addNotificationBadge(pendingRewards.length);
@@ -585,19 +475,11 @@ function handleCheckmarkClick(itemIndex) {
   addNotificationBadge(pendingRewards.length);
 }
 
-function updateChordiiSummary() {
-  let totalChordii = 0;
-
-  // Iterate through the data to calculate totals
-  Object.values(caloriesData).forEach((dayData) => {
-    if (dayData.chordii) {
-      totalChordii += Number(dayData.chordii);
-    }
-  });
-
-  let chordiiRetail = totalChordii * 15;
+function updateSummary() {
+  let totalDays = countDaysMeetingCriteria();
+  let daysTracked = Object.entries(caloriesData).length;
 
   // Update the summary paragraph
   const summaryElement = document.getElementById("summary");
-  summaryElement.innerHTML = `Total Chordii Assembled: ${totalChordii} ($${chordiiRetail.toLocaleString()})`;
+  summaryElement.innerHTML = `Days w/ all goals: ${totalDays} / ${daysTracked}`;
 }
